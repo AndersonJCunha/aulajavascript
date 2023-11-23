379a02e69a7aa98afce821f72ed7c1b6bacc10d5
Anderson jose da cunha <andersonjc244@gmail.com>
2023-11-23 20:46:53 -0300

    certo

--- aula_03/assets/js/strings.js
+++ aula_03/assets/js/strings.js
@@ -1,43 +1,43 @@
-const renderiza_string = function (elemento) {
-    //console.log(elemento.value);
-    //console.log(elemento.value.trin());
-    const valor = elemento.value;
-    console.log(valor.split('/'));
-    console.log(elemento.value.valueOf());
-   // const vetor_da_string = valor.split('/');
-    console.log(vetor_da_string);
-    const vetor_da_string = valor.split('@');
-    //const string_do_valor = vetor_da_string.join(' - ')
-    console.log(string_do_valor);
-    const outro_valor = valor.concat(" ...")
-    console.log(vetor_da_string);
-    
+const renderiza_string = function(elemento){
+    const valor = elemento.value;  
+    const outro_valor = valor.concat("...");
+    const vetor_da_string = valor.split('/');
+    const tamanho_endereço = valor.split('@')[0].length;
+    const string_do_vetor = vetor_da_string.join('-');
+    const email = 'andersonjc244@gmail.com.br';
+    const paddedEmail = valor.substring(0,2).padEnd(tamanho_endereço,'*') + valor.substring(tamanho_endereço);
    
-    
-    
-
-
-
-
-    const render = document.getElementById('render_string');
-    render.innerHTML = `
-    <p>${valor.toUpperCase()}<p>
-    <p>${valor.toLowerCase()}<p>
-    <p>${valor.substring(0, 4)} ...<p>
-    <p>COMEÇA COM SCRIPT? ${valor.startsWith("SCRIPT")}</p>
-    <p>TERMINA COM LEGAL? ${valor.endsWith("LEGAL")}</p>
-    <p>EXISTE A PALAVRA CHUVA? ${valor.trim().toUpperCase().includes("CHUVA")}</p>
-    <p>${outro_valor}</p>
-    <p>${valor.replace("[email]", "andersonjc244@gmail.com")}</p>
-    <p>${valor.replaceAll("[telefone]", '48425-5722')}</p>
-    <p>${valor.repeat(20)}</p>
-    <p>${valor.padEnd(10, '*')}</p>
-    <p>${valor.padStart(10, '#')}</p>
-    <p>A texto <b>${valor}</b> posui ${valor.length}caracteres</p>
-    <p> A texto <b>${valor}</b> possui ${valor.length}caracteres</p>
-    <p>${maskedNumber}</p>
-    
-    
-    `;
-
+   console.log(elemento.value);
+   console.log(elemento.value.trim());
+   console.log(elemento.value.valueOf());
+   console.log(valor.split('/'));
+   console.log(vetor_da_string);
+   console.log(string_do_vetor);
+   console.log(paddedEmail);
+ 
+ const render = document.getElementById('render_string');
+   render.innerHTML = `
+      <p>${valor.toUpperCase()}<p>
+      <p>${valor.toLowerCase()}<p>
+      <p>${valor.substring(0,4)}....<p>
+      <p>Começa com Script? ${valor.startsWith("script")}</p>
+      <p>Termina com legal? ${valor.endsWith("legal")}</p>
+      <p>Existe a palavra chuva? ${valor.trim().toLowerCase().includes("chuva")}</p> <!-- -->
+      <p>${outro_valor}</p>
+      <p>${valor.replace("[email]","andersonjc244@gmail.com.br")}</p>
+      <p>${valor.replaceAll("[telefone]",'98425-5722')}</p>
+      <p>${valor.repeat(60)}</p>
+      <p>${valor.padStart(3,4,'**')}</p>
+      <p> A texto <b>${valor}</b> possui ${valor.length}caracteres</p>
+ 
+     
+ 
+ 
+ 
+ 
+   
+ 
+     
+   `;
+   
 }
